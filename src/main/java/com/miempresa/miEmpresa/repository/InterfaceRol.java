0bd62fe5d4781b0dc256ce0abdc88efea8091699
tepla18 <93670533+tepla18@users.noreply.github.com>
2022-09-02 16:59:08 -0500

    eliminacion de rol

--- src/main/java/com/miempresa/miEmpresa/repository/InterfaceRol.java
+++
@@ -1,9 +0,0 @@
-package com.miempresa.miEmpresa.repository;
-
-import com.miempresa.miEmpresa.entities.RolModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InterfaceRol extends JpaRepository<RolModel, Integer> {
-}
